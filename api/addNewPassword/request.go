9c7481139d943924531401eb17680802e142543c
ProgrammierIgel <144588240+ProgrammierIgel@users.noreply.github.com>
2025-04-17 13:50:51 +0200

    refactor add new password

--- api/addNewPassword/request.go
+++ api/addNewPassword/request.go
@@ -1,10 +1,14 @@
 package addnewpassword
 
 type RequestBody struct {
-	PasswordName   string `json:"passwordName"`
-	Password       string `json:"passwordToAdd"`
-	AccountName    string `json:"accountName"`
-	MasterPassword string `json:"accountPassword"`
-	URL            string `json:"url"`
-	Username       string `json:"username"`
+	PasswordName   string   `json:"passwordName"`
+	Password       Password `json:"passwordToAdd"`
+	AccountName    string   `json:"accountName"`
+	MasterPassword string   `json:"accountPassword"`
+}
+
+type Password struct {
+	Password string `json:"password"`
+	URL      string `json:"url"`
+	Username string `json:"username"`
 }
